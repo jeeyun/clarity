a137836638d53b26bf89cc705dfe66dd41ccfa4d
Jeeyun Lim <jeeyun.lim@gmail.com>
2016-11-29 15:25:06 -0800

    [core] Adding Backstop for CSS regression testing
    Signed-off-by: Jeeyun Lim <jeeyun.lim@gmail.com>

--- backstop_data/casper_scripts/onReadyHomepage.js
+++ backstop_data/casper_scripts/onReadyHomepage.js
@@ -1,5 +1,5 @@
 module.exports = function(casper, scenario, vp) {
-    casper.echo('onReadyHomePage.js', 'INFO');
+    casper.echo('onReadyHomepage.js', 'INFO');
     casper.wait(1000);
     casper.then(function() {
         this.mouse.click(".nav-icon");
